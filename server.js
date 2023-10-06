ae1ecfbec95ba5889c6efc27815c6dde91397de8
kdima31 <dimakanso16@gmail.com>
2023-10-06 22:27:00 +0300

    music

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
+import musicRoute from "./routes/musicRoute.js";
 import connectDb from "./db.js";
 
 dotenv.config();
@@ -12,10 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+
 connectDb();
 
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/music", musicRoute);
 
 app.listen(process.env.PORT, (error) => {
   if (!error) {
