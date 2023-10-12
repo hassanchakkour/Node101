eddbb6ddbb2baa3e1c667ca8f93352e09df7ed77
hassanchakkour <hassan.chakkour@hotmail.com>
2023-10-12 22:04:52 +0300

    main changes hisham

--- controller/bookscontroler.js
+++ controller/bookscontroler.js
@@ -1,67 +1,101 @@
 import Book from "../models/booksmodel.js";
 
-const createBook = async (req, res) => {
-    const { title, pages } = req.body;
+//crud operation
+// C => create
+// r => read
+// u => update
+// d => delete
 
+const createBook = async (req, res) => {
+  const { title, pages, password } = req.body;
+
+  const alredyExist = await Book.findOne({ title });
+
+  if (alredyExist) {
+    res.json({ message: "Book Already Exist !!" });
+  } else {
     const newBook = await Book.create({
-        title,
-        pages,
+      title,
+      pages,
+      password,
     });
 
-    console.log(newBook);
     res.status(201).json(newBook);
+  }
 };
 
 const getBooks = async (req, res) => {
-    const books = await Book.find({});
+  const books = await Book.find({});
 
-    if (books) {
-        res.status(200).json(books);
-    } else {
-        res.status(404).json({ Error: "No data" });
-    }
+  if (books) {
+    res.status(200).json(books);
+  } else {
+    res.status(404).json({ Error: "No data" });
+  }
 };
 
 const getSingleBook = async (req, res) => {
-    const title = req.params.title;
+  const title = req.params.title;
 
-    const singleBook = await Book.findOne({ title });
+  const singleBook = await Book.findOne({ title });
 
-    if (singleBook) {
-        res.json(singleBook);
-        console.log(singleBook);
-    } else {
-        res.status(404).json({ Error: "Book not found" });
-    }
+  if (singleBook) {
+    res.json(singleBook);
+    console.log(singleBook);
+  } else {
+    res.status(404).json({ Error: "Book not found" });
+  }
 };
 
 const deleteBook = async (req, res) => {
-    const { title } = req.body;
+  const { title } = req.body;
 
-    const deletedBook = await Book.findOneAndRemove({ title });
+  const deletedBook = await Book.findOneAndRemove({ title });
 
-    if (deletedBook) {
-        res.status(200).json({ Message: "Book deleted successfully" });
-    } else {
-        res.status(404).json({ Error: "Book not found" });
-    }
+  if (deletedBook) {
+    res.status(200).json({ Message: "Book deleted successfully" });
+  } else {
+    res.status(404).json({ Error: "Book not found" });
+  }
 };
 
 const updateBook = async (req, res) => {
-    const { title, pages } = req.body;
+  const { title, pages, password } = req.body;
 
-    const existingBook = await Book.findOne({ title });
+  const existingBook = await Book.findOne({ title });
 
-    if (existingBook) {
-        existingBook.title = title || existingBook.title;
-        existingBook.pages = pages || existingBook.pages;
+  if (existingBook) {
+    existingBook.title = title || existingBook.title;
+    existingBook.pages = pages || existingBook.pages;
+    existingBook.password = password || existingBook.password;
 
-        await existingBook.save();
+    await existingBook.save();
 
-        res.status(200).json(existingBook);
-    } else {
-        res.status(404).json({ Error: "Book not found" });
-    }
+    res.status(200).json(existingBook);
+  } else {
+    res.status(404).json({ Error: "Book not found" });
+  }
 };
 
-export { createBook, getBooks, getSingleBook, deleteBook, updateBook };
+// @ POST
+// @ Login
+const loginBook = async (req, res) => {
+  const { title, password } = req.body;
+
+  const book = await Book.findOne({ title });
+
+  if (book && (await book.hishamPassword(password))) {
+    res.json({ message: "Login Successfull" });
+  } else {
+    res.json({ message: "mesh mawjoud" });
+  }
+};
+
+export {
+  createBook,
+  getBooks,
+  getSingleBook,
+  deleteBook,
+  updateBook,
+  loginBook,
+};
--- models/booksmodel.js
+++ models/booksmodel.js
@@ -1,16 +1,43 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const bookSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    pages: {
-        type: Number,
-        required: true,
-    }
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  pages: {
+    type: Number,
+    required: true,
+  },
 });
+
+// schema name
+// previous abel l "save"
+
+bookSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // generate salt == keb 3lya bharat
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// schema name
+// hayde mothod
+// esem l method
+
+bookSchema.methods.hishamPassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
 
 const Book = mongoose.model("Book", bookSchema);
 
